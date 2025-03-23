81930f21417de3e3ccd8cc463fc92074b8cf0c27
Preetam <preetamdas248@gmail.com>
2025-03-23 15:07:24 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route  path="/" element={<App/>} />
+        <Route path="/slack-clone" element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
